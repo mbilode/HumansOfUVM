ba69dd1c5f3af122bd670908d3b08ca6ed86681d
mbilode <mlbilode@uvm.edu>
2014-10-28 20:53:47 -0400

    erasing all files
    yup..

--- midSection.php
+++
@@ -1,8 +0,0 @@
-
-
-<ul id="midSecNav">
-    
-    <li><a href="https://www.uvm.edu/~mlbilode/HumansOfUVM/home.php"><img style="border:0;" src="arrow.png" alt="HTML tutorial"><h3>Twitter</h3></a><p>Israel and the militant group Hamas seemed set on a collision course on Monday, with an escalation of cross-border clashes around the Gaza Strip, Hamas vowing to avenge the deaths of six of its fighters, and preparations underway for a possible large-scale Israeli operation in the Palestinian coastal territory.</p></li>
-    <li><a href="https://www.uvm.edu/~mlbilode/HumansOfUVM/home.php"><img style="border:0;" src="arrow.png" alt="HTML tutorial"><h3>Facebook</h3></a><p>Lt. Col. Peter Lerner, a spokesman for the Israeli military, said the army was completing the deployment of two infantry brigades along the border with Gaza and had approved the call-up of 1,500 reservists, mainly Home Front Command and aerial defense units.</p></li>
-    <li><a href="https://www.uvm.edu/~mlbilode/HumansOfUVM/home.php"><img style="border:0;" src="arrow.png" alt="HTML tutorial"><h3>Instagram</h3></a><p>“If last week we were talking about calm being answered by calm,” Colonel Lerner said, “we are now talking about preparing for an escalation.”</p></li>
-</ul>
