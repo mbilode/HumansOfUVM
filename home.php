09522f18ccce0cd4a4cc913287b0e1a517a3b5ef
mbilode <mlbilode@uvm.edu>
2014-11-20 19:06:08 -0500

    November 20 css
    random css changes

--- home.php
+++ home.php
@@ -1,6 +1,5 @@
   <?php>include("top.php")?>
-  
-  
+
   <body>
       
       
--- top.php
+++ top.php
@@ -1,21 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Humans of UVM</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+  
     <head>
         <title>Form</title>
         <meta charset="utf-8">
@@ -28,7 +13,7 @@
         <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
         <![endif]-->
 
-        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="css/monica.css" type="text/css" media="screen">
 
         <?php
         $debug = false;
