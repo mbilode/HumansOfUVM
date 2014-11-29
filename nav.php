593bed906d09e3202b49f84d9c613942fbf668df
miicch <mmarin@uvm.edu>
2014-11-29 15:09:26 -0500

    Search kinda works now

--- nav.php
+++ nav.php
@@ -13,10 +13,6 @@
                 
         </li>
         <li><a href="Contact.php">Contact</a></li>
-<<<<<<< HEAD
         <li><?php include 'search.php'; ?></li>
-=======
-        <li>Search----------</li>
->>>>>>> f4d38794f00f46ac82799fa9ed80d6799b9a3e5b
     </ul>
 </nav>    
--- search.php
+++ search.php
@@ -1,63 +1,75 @@
-<?php
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+
+<link href="css/lightbox.css" rel="stylesheet" />
+<div class="image-row"> 
+    <?php
+
 /// FORM VARIABLES //
-$search = "";
+    $search = "";
 
 
 //// CHECK FOR SUBMIT ////
-if (isset($_POST["btnSubmit"])) {
+    if (isset($_POST["btnSubmit"])) {
 
-    $debug = false;
+        $debug = false;
 
-    if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
-        $debug = true;
+        if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
+            $debug = true;
+        }
+
+        if ($debug)
+            print "<p>DEBUG MODE IS ON</p>";
+
+        /* if (!securityCheck(true)) {
+          $msg = "<p>Sorry you cannot access this page. ";
+          $msg.= "Security breach detected and reported</p>";
+          die($msg);
+          } */
+
+        /// SANITIZE ///
+        $search = htmlentities($_POST["txtSearch"], ENT_QUOTES, "UTF-8");
     }
-
-    if ($debug)
-        print "<p>DEBUG MODE IS ON</p>";
-
-    /*if (!securityCheck(true)) {
-        $msg = "<p>Sorry you cannot access this page. ";
-        $msg.= "Security breach detected and reported</p>";
-        die($msg);
-    }*/
-
-    /// SANITIZE ///
-    $search = htmlentities($_POST["txtSearch"], ENT_QUOTES, "UTF-8");
-}
 
     //QUERY ///
 
-    $query = "SELECT * FROM tblImage WHERE fnkCategoryId = ?";
+    $query = "SELECT fldName FROM tblImage WHERE fnkCategoryId LIKE ?";
     $data = array($search);
 
     $results = $dbh->select($query, $data);
-
-  
-
-
-    /// DISPLAY ///
-
+    //print_r($results);
 
     ?>
-        <form action="<?php print $phpSelf; ?>"
-              method="post"
-              id="frmSearch">
+    <form action="<?php print $phpSelf; ?>"
+          method="post"
+          id="frmSearch">
 
 
 
-            <label for="txtSearch" class="required">
-                <input type="text" id="txtSearch" name="txtSearch"
-                       value="<?php print $search; ?>"
-                       tabindex="100" maxlength="45" placeholder="Search"
-                       onfocus="this.select()"
-                       autofocus>
-                <input type="submit" id="btnSubmit" name="btnSubmit" value="Search" tabindex="900" class="button">
-            </label>
+        <label for="txtSearch" class="required">
+            <input type="text" id="txtSearch" name="txtSearch"
+                   value="<?php print $search; ?>"
+                   tabindex="100" maxlength="45" placeholder="Search"
+                   onfocus="this.select()"
+                   autofocus>
+            <input type="submit" id="btnSubmit" name="btnSubmit" value="Search" tabindex="900" class="button">
+        </label>
 
+    </form>
+    <?php
+    /// DISPLAY ///
+    $folder = "HumanPics";
 
-
-
-        </form>
+foreach ($results as $row) {
+    foreach ($row as $field => $value) {
+            if (!is_int($field)) {
+                echo '<a class="example-image-link" rel="lightbox" href="' . $folder . '/' . $value . '"data-lightbox="'. $value .'">';
+    echo '<img class="' . $folder . '/' . $value . '" src="' . $folder . '/' . $value . '" alt="Humans" />' . "\n";
+    echo "</a>";
+            }
+        }
+}
+?>
 
 
 
